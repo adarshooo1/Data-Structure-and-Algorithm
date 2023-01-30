7bbe1527ecc41ae46dbeb44c596c295b7e750e10
Adarsh Singh <adarshsinghak001@gmail.com>
2023-01-31 00:57:36 +0530

    Leetcode Question Submission

---
+++ Array/Sentence_Is_Pangram.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Sentence_Is_Pangram {
+    public static void main(String[] args) {
+        String sentence = "qwertyuioplkjhgfdsazxcvbnm";
+        boolean checker = checkIfPangram(sentence);
+        System.out.println(checker);
+    }
+    static boolean checkIfPangram(String sentence) {
+        if (sentence.length() < 26) {
+            return false;
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (sentence.indexOf(c) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
